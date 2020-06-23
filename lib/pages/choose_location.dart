f2337372e5e53b9b30b0d4c556b525d1d3b9c9a1
Uki7991 <tilek.kubanov@gmail.com>
2020-06-23 15:35:21 +0600

    http

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,27 +7,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-    String name = await Future.delayed(Duration(seconds: 3), () {
-      return 'Tilek';
-    });
-
-    String bio = await Future.delayed(Duration(seconds: 3), () {
-      return 'bio';
-    });
-    
-    print('$name - $bio');
-  }
-
-  int counter = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('statement');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,14 +17,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: RaisedButton(
-        onPressed: () {
-          setState(() {
-            counter++;
-          });
-        },
-        child: Text('$counter'),
-      )
     );
   }
 }
